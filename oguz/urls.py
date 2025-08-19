e2f1fd3212873fa91ac662adc48fbc923b84ffea
Teymurlu Yusif <teymurluyusfi555@gmail.com>
2025-08-19 19:57:11 +0400

    updated

--- oguz/urls.py
+++ oguz/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('', include('core.urls')),
     path('', include('courses.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
