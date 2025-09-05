150e91688ae397d0518143c271a6b7a9d59a6c24
Teymurlu Yusif <teymurluyusfi555@gmail.com>
2025-09-05 20:02:30 +0400

    updated

--- core/models.py
+++ core/models.py
@@ -15,8 +15,7 @@ class Contact(AbstractModel):
     last_name = models.CharField('lastname',max_length=100)
     email = models.EmailField('email',unique=False)
     phone = models.CharField('phone',max_length=15, blank=True, null=True)
-    message = models.TextField('message',unique=False)
-
+    message = models.TextField('message', unique=True)
     def __str__(self):
         return self.first_name + ' ' + self.last_name
     
